3c528477d48e056f904f7716acfa60975e8a192f
dipika <b.dipika106@gmail.com>
2020-09-28 10:01:59 +1000

    Initial Commit

---
+++ AnimationButton/AnimationButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationButtonApp.swift
+//  AnimationButton
+//
+//  Created by Dipika on 28/9/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimationButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
